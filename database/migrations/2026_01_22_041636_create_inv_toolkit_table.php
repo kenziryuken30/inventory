5839b85cdd6331c4d175688a2ad17304b40b593b
kenziryuken30 <ryukesyauqi30@gmail.com>
2026-02-02 10:47:58 +0700

    perbarui

--- database/migrations/2026_01_22_041636_create_inv_toolkit_table.php
+++ database/migrations/2026_01_22_041636_create_inv_toolkit_table.php
@@ -12,19 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('inv_toolkit', function (Blueprint $table) {
-            $table->string('id',20)->primary();
-            $table->string('toolkit_name',200);
-            $table->string('category_id',20);
-            $table->string('image',255)->nullable();
+    $table->string('id', 20)->primary();
+    $table->string('toolkit_name', 200);
+    $table->string('category_id', 20);
+    $table->string('image')->nullable();
+    $table->enum('status', ['tersedia', 'dipinjam', 'rusak', 'maintenance'])
+          ->default('tersedia');
 
-            $table->enum('status', ['tersedia','dipinjam','rusak','maintenance'])
-      ->default('tersedia');
-            $table->timestamps();
-            $table->softDeletes();
-    
-
-            $table->timestamps();
-
+    $table->timestamps();     // created_at & updated_at
+    $table->softDeletes();    // deleted_at
 
             $table->foreign('category_id')->references('id')->on('inv_category')->cascadeOnUpdate();
         });
